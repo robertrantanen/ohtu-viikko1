a1b7463df9345836b8a8a88f6b4f0a0cb329cec8
Robert <robert.j.rantanen@helsinki.fi>
2019-10-29 22:07:33 +0200

    korjattu testi

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -30,7 +30,7 @@ public class VarastoTest {
 
     @Test
     public void konstruktoriLuoTyhjanVaraston() {
-        assertEquals(1, varasto.getSaldo(), vertailuTarkkuus);
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
     }
     
     @Test
