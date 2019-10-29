6427c8889afe6d37ee29a01119112298e36824a1
Robert <robert.j.rantanen@helsinki.fi>
2019-10-29 22:35:32 +0200

    parempi testikattavuus

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,9 +1,10 @@
-package ohtu.ohtuvarasto;
+package main;
+
+import ohtu.ohtuvarasto.Varasto;
 
 public class Main {
 
     public static void main(String[] args) {
-
         
         Varasto mehua = new Varasto(100.0);
         Varasto olutta = new Varasto(100.0, 20.2);
